f745b50d8c2372ddd26679357c428183a060dad6
usasergio <usasergio2@gmail.com>
2021-11-22 18:07:11 +0100

    Arreglado CSS, circulos en JS y minijuego COORDS

--- js/script.js
+++ js/script.js
@@ -12,12 +12,22 @@
 const INICIO = 0;
 const PRIMSECCION = 0;
 
+let scrollDoc;
+let docHeight;
+let winHeight;
+
+let mapa = document.querySelector(".minijuego img");
+let isMGStarted = false;
+
+
 window.onload = iniciar;
 
 window.addEventListener("scroll",progreso);
-window.onclick = clicks;
 
 function iniciar() {
+
+    //Inicio minijuego
+    mapa.onclick = miniGameClicks;
 
     //Slider inicial
     let slide = 0;
@@ -27,7 +37,16 @@ function iniciar() {
 
     //Muestra el total de elementos que hay.
     let total = slides.length-1;
-    
+
+    //Calculo cuantas secciones hay y creo puntos.
+    let seccionesTotal = document.querySelectorAll("main > div").length;
+    for(let i=0;i<seccionesTotal;i++) {
+        let puntos = document.createElement("a");
+        puntos.classList.add("puntos");
+        document.getElementById("enlaces").appendChild(puntos);
+    }
+     
+
     // Muestra el primer slider y el primer punto
     showSlider(slide);
     document.querySelectorAll("nav .puntos")[0].style.backgroundColor = "black";
@@ -83,12 +102,12 @@ function iniciar() {
  * Calcula el porcentaje de scroll que has hecho en la página y lo muestra en una barra.
  */
 function progreso() {
-    let scroll = document.documentElement.scrollTop;
-    let docHeight = document.documentElement.scrollHeight;
-    let winHeight = window.innerHeight;
+    scrollDoc = document.documentElement.scrollTop;
+    docHeight = document.documentElement.scrollHeight;
+    winHeight = window.innerHeight;
     
 
-    let porcentaje = (scroll) / (docHeight-winHeight);
+    let porcentaje = (scrollDoc) / (docHeight-winHeight);
 
     let total = Math.round(porcentaje*100);
 
@@ -106,7 +125,10 @@ function progreso() {
         document.querySelector("nav").style.backgroundColor = "#4C566A";
         navPuntos(0);
     }
+    else if(total > 37)
+        window.scrollTo(45,0);
     document.getElementById("barra").style.width = total+"%";
+    
 }
 
 /**
@@ -121,4 +143,36 @@ function navPuntos(n) {
 
     //Ponemos el color de nuestro punto
     document.querySelectorAll("nav .puntos")[n].style.backgroundColor = "black";
+}
+
+/**
+ * Función de clicks del minijuego principal.
+ */
+function miniGameClicks(event) {
+
+    //Inicio minijuego
+    isMGStarted = true;
+
+    //Bloqueo drags
+    mapa.addEventListener("dragstart", (event) => event.preventDefault());
+
+    //Vars de bloque
+    let div = document.getElementsByClassName("minijuego")[0];
+    let medidas = parseFloat(docHeight-winHeight);
+
+    //Primer inicio, poner animación.
+    if(isMGStarted){
+        console.log("Iniciando minijuego...");
+        mapa.src = "recursos/minigame.jpg";
+
+        let regalo = document.createElement("img");
+        regalo.src = "recursos/imgs/giftbox.png";
+        regalo.classList.add("regalo");
+        div.appendChild(regalo);
+
+        regalo.style.top = `${medidas+event.clientY}px`;
+        regalo.style.left = `${event.clientX}px`;
+    }
+    
+    //console.log(event.target);
 }
